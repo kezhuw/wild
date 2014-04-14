ee9dad29802d85ea427dfc78a12b0b5ad390bdbf
Kezhu Wang <kezhuw@gmail.com>
2014-04-14 23:40:57 +0800

    rewrite stack trace Exception

--- exception.cpp
+++ exception.cpp
@@ -1,34 +1,12 @@
 #include "exception.hpp"
 #include "misc.hpp"
 
+#include <utility>
+
 namespace wild {
 
-Exception::Exception(std::string message, const Exception *innerException)
-    : _message(message), _stackTrace(::wild::StackTrace(2)), _innerException(innerException) {
-}
-
-Exception::Exception(std::string message, std::string stackTrace, const Exception *innerException)
-    : _message(message), _stackTrace(stackTrace), _innerException(innerException) {
-}
-
-const char *
-Exception::what() const noexcept {
-    return Message().c_str();
-}
-
-const std::string&
-Exception::Message() const noexcept {
-    return _message;
-}
-
-const std::string&
-Exception::StackTrace() const noexcept {
-    return _stackTrace;
-}
-
-const Exception *
-Exception::InnerException() const noexcept {
-    return _innerException;
+Exception::Exception(std::string message)
+    : _message(std::move(message)), _stackTrace(::wild::StackTrace(2)) {
 }
 
 }
--- exception.hpp
+++ exception.hpp
@@ -4,26 +4,52 @@
 #include <exception>
 #include <string>
 
+// Exception with stack trace.
+
 namespace wild {
 
 class Exception : std::exception {
 public:
 
-    explicit Exception(std::string message, const Exception *inner = nullptr);
+    explicit Exception(std::string message);
 
-    Exception(std::string message, std::string stackTrace, const Exception *inner = nullptr);
+    explicit Exception(std::string stackTrace, std::string message)
+        : _message(std::move(message)), _stackTrace(std::move(stackTrace)) {
+    }
 
-    virtual const char *what() const noexcept override;
+    virtual const char *what() const noexcept override {
+        return _message.c_str();
+    }
 
-    const std::string& StackTrace() const noexcept;
-    const std::string& Message() const noexcept;
-    const Exception *InnerException() const noexcept;
+    const std::string& Message() const noexcept {
+        return _message;
+    }
+
+    const std::string& StackTrace() const noexcept {
+        return _stackTrace;
+    }
+
+    std::exception_ptr InnerException() const noexcept {
+        return _innerException;
+    }
+
+    void SetInnerException(std::exception_ptr inner) noexcept {
+        _innerException = std::move(inner);
+    }
+
+    // copyable
+    Exception(const Exception&) = default;
+    Exception& operator=(const Exception&) = default;
+
+    // movable
+    Exception(Exception&&) = default;
+    Exception& operator=(Exception&&) = default;
 
 private:
 
     std::string _message;
     std::string _stackTrace;
-    const Exception *_innerException;
+    std::exception_ptr _innerException;
 };
 
 }
