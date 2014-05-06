9940a863da56edf782bb88dfb69031c06a188d69
Kezhu Wang <kezhuw@gmail.com>
2014-05-06 23:08:09 +0800

    type safe version forward list

--- forward_list.hpp
+++ forward_list.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
+#include <vector>
+
 namespace wild {
 
 class ForwardList {
 public:
 
-    ForwardList()
+    ForwardList() noexcept
         : _tail(&_dummy) {} 
 
-    ForwardList(ForwardList&& other)
+    ForwardList(ForwardList&& other) noexcept
         : _dummy(other._dummy.Next()), _tail(other._tail) {
-        other.reset();
+        other.clear();
     }
 
     ForwardList(const ForwardList&) = delete;
@@ -19,12 +21,10 @@ public:
     ForwardList& operator=(ForwardList&& other) = delete;
 
     struct Node {
-        explicit Node(Node *next = nullptr) : _next(next) {}
+        explicit Node(Node *next = nullptr) noexcept : _next(next) {}
 
         struct Node *Next() { return _next; }
         void SetNext(Node *next) { _next = next; }
-
-        friend ForwardList;
 
     private:
         struct Node *_next;
@@ -45,44 +45,84 @@ public:
         return _tail;
     }
 
-    void push(Node *node) {
-        _tail->SetNext(node);
-        _tail = node;
-    }
-
-    Node *pop() {
+    Node *take() {
         Node *head = _dummy.Next();
         if (head == nullptr) {
             return nullptr;
         }
         if (head == _tail) {
-            reset();
+            clear();
         } else {
             _dummy.SetNext(head->Next());
         }
         return head;
     }
 
-    void push_back(Node *node) {
-        push(node);
+    void push(Node *node) {
+        _tail->SetNext(node);
+        _tail = node;
     }
 
-    void pop_front() {
-        pop();
+    void pop() {
+        take();
     }
 
-    Node *nodes() {
+    template<typename T>
+    static T* nodeT(Node *node, uintptr offset) {
+        return reinterpret_cast<T*>(reinterpret_cast<uintptr>(node) - offset);
+    }
+
+    template<typename T>
+    T* frontT(uintptr offset) {
+        if (auto node = front()) {
+            return nodeT<T>(node, offset);
+        }
+        return nullptr;
+    }
+
+    template<typename T>
+    void pushT(T* t, Node T::* field) {
+        push(&(t->*field));
+    }
+
+    template<typename T>
+    T* backT(uintptr offset) {
+        if (empty()) {
+            return nullptr;
+        }
+        return nodeT<T>(_tail, offset);
+    }
+
+    template<typename T>
+    T* takeT(uintptr offset) {
+        Node *head = front();
+        if (head == nullptr) {
+            return nullptr;
+        }
+        if (head == _tail) {
+            clear();
+        } else {
+            _dummy.SetNext(head->Next());
+        }
+        return nodeT<T>(head, offset);
+    }
+
+    template<typename T>
+    std::vector<T*> vectorT(uintptr offset) {
         _tail->SetNext(nullptr);
-        return _dummy.Next();
+        ForwardList::Node *head = front();
+        if (head) {
+            std::vector<T*> elems;
+            do {
+                elems.push_back(nodeT<T>(head, offset));
+            } while ((head = head->Next()));
+            elems.shrink_to_fit();
+            return elems;
+        }
+        return nullptr;
     }
 
-    Node *clear() {
-        Node *l = nodes();
-        reset();
-        return l;
-    }
-
-    void reset() {
+    void clear() {
         _dummy.SetNext(nullptr);
         _tail = &_dummy;
     }
@@ -91,5 +131,50 @@ private:
     struct Node _dummy;
     struct Node *_tail;
 };
+
+template<typename T, ForwardList::Node T::* field>
+class ForwardListT {
+public:
+
+    bool empty() {
+        return _list.empty();
+    }
+
+    T* front() {
+        return _list.frontT<T>(_offset);
+    }
+
+    T* back() {
+        return _list.backT<T>(_offset);
+    }
+
+    T* take() {
+        return _list.takeT<T>(_offset);
+    }
+
+    void push(T *t) {
+        _list.pushT(t, field);
+    }
+
+    void pop() {
+        _list.pop();
+    }
+
+    void clear() {
+        _list.clear();
+    }
+
+    std::vector<T*> vector() {
+        return _list.vectorT<T>(_offset);
+    }
+
+private:
+
+    static uintptr _offset;
+    ForwardList _list;
+};
+
+template<typename T, ForwardList::Node T::* field>
+uintptr ForwardListT<T, field>::_offset = reinterpret_cast<size_t>(&(static_cast<T*>(nullptr)->*field));
 
 }
