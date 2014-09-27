d196ca415d22fbebe47d09693c6451e4034278ed
Kezhu Wang <kezhuw@gmail.com>
2014-09-27 08:50:33 +0800

    using C++11's type alias to define type aliases

--- types.hpp
+++ types.hpp
@@ -10,30 +10,28 @@ namespace wild {
 
 namespace types {
 
-typedef std::int8_t int8;
-typedef std::uint8_t uint8;
+using int8      = std::int8_t;
+using int16     = std::int16_t;
+using int32     = std::int32_t;
+using int64     = std::int64_t;
+using intptr    = std::intptr_t;
+using intreg    = intptr;
 
-typedef std::int16_t int16;
-typedef std::uint16_t uint16;
+using uint8     = std::uint8_t;
+using uint16    = std::uint16_t;
+using uint32    = std::uint32_t;
+using uint64    = std::uint64_t;
+using uintptr   = std::uintptr_t;
+using uintreg   = uintptr;
 
-typedef std::int32_t int32;
-typedef std::uint32_t uint32;
+using uint      = unsigned int;
 
-typedef std::int64_t int64;
-typedef std::uint64_t uint64;
-
-typedef std::intptr_t intptr;
-typedef std::uintptr_t uintptr;
-
-typedef unsigned int uint;
-
-typedef intptr intreg;
-typedef uintptr uintreg;
+using byte_t    = uint8;
+using size_t    = std::size_t;
 
 using pointer_t = struct _ {int _;} *;
-using func_t = void (*)(pointer_t);
-using byte_t = uint8;
-using std::size_t;
+using func_t    = void (*)(pointer_t);
+
 using std::string;
 
 }
