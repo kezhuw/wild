63a0a60f2dac0467d5bb7108b4db387374f65872
Kezhu Wang <kezhuw@gmail.com>
2015-04-12 21:56:52 +0800

    fix compiling error on FreeBSD

--- misc.cpp
+++ misc.cpp
@@ -4,12 +4,18 @@
 
 #include <stdlib.h>
 
+#if defined(__FreeBSD__)
+using backtrace_len_t = size_t;
+#else
+using backtrace_len_t = int;
+#endif
+
 namespace wild {
 
 string
 StackTrace(uintreg level) {
     void *stacks[128];
-    int n = ::backtrace(stacks, static_cast<int>(nelem(stacks)));
+    backtrace_len_t n = ::backtrace(stacks, static_cast<backtrace_len_t>(nelem(stacks)));
     char **symbols = ::backtrace_symbols(stacks, n);
     if (symbols == NULL) {
         return "(nil stack)\n";
