e11b0fa758f26202eaea351f37fdf41f47cd6d85
Kezhu Wang <kezhuw@gmail.com>
2015-04-21 10:58:20 +0800

    remove default argument version of wild::take, seems error-prone

--- utility.hpp
+++ utility.hpp
@@ -25,34 +25,4 @@ auto take_back(C& c) {
     return v;
 }
 
-template<typename C, typename T>
-auto take(C& c, T&& default_value) -> typename C::value_type {
-    if (c.empty()) {
-        return std::forward<T>(default_value);
-    }
-    auto v = std::move(c.front());
-    c.pop();
-    return v;
-}
-
-template<typename C, typename T>
-auto take_front(C& c, T&& default_value) -> typename C::value_type {
-    if (c.empty()) {
-        return std::forward<T>(default_value);
-    }
-    auto v = std::move(c.front());
-    c.pop_front();
-    return v;
-}
-
-template<typename C, typename T>
-auto take_back(C& c, T&& default_value) -> typename C::value_type {
-    if (c.empty()) {
-        return std::forward<T>(default_value);
-    }
-    auto v = std::move(c.back());
-    c.pop_back();
-    return v;
-}
-
 }
